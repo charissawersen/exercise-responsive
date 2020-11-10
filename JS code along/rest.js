0aab92a4a974087c600ac72cbe82b95d4bb3fa55
Charissa Wersen <charissa.wersen@gmail.com>
2020-11-09 19:44:20 -0500

    code along updates

---
+++ JS code along/rest.js
@@ -0,0 +1,16 @@
+// Pretend that console.log only takes in one argument
+
+// Create a function that takes any amount of arguments.
+// call console.log with each of those arguments.
+
+// console.log(thing1, thing2, thing3); 
+
+const thing1 = 1;
+const thing2 = 2;
+const thing3 = 3;
+function consoleAll (...everythingElse){
+    let response = '';
+    for(everythingElse of everythingElses) {
+        console.log (everythingElse);
+    }
+}
--- Objects/Arrays Exercise 1/script.js
+++ Objects/Arrays Exercise 1/script.js
@@ -1,4 +1,4 @@
-const character = [
+const characters = [
     {
         name: "Bear",
         health: 5
@@ -14,9 +14,38 @@ const character = [
 ]
 const opponent = { name: "Tiger", health: 8 }
 
-for ()
+// while(true) {
+// for (let i = 0; i < character.length; i++) {
+//     console.log(character[i]);
+//     console.log(opponent);
+//     let player = prompt("Choose player 1, 2, or 3"); 
+//     if (player === 1) {
+//         if (character.name == "Bear") {
+//             character.find(function (character)) {
+//                 character.health[0] -= 5;
+//             };
+//         }
+    
+//         // let battle;
+//         // character[0].health = character[0].health - 5;
+//         // opponent.health = opponent.health - 5;
+//     };
+// };
+// }  
+// console.log(character[0], opponent);
 
+let response = prompt("select a character (1, 2, or 3)");
+const characterIndex = parseInt(response);
+characters[characterIndex].health -=5;
+opponent.health -=5; 
 
+do {
+    for (character of characters){
+        console.log(character.name, character.health);
+    }
+    console.log(opponent.name, opponent.health);
+
+} while ( response !== '')
 
 
 // let found;
